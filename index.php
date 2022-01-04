ccdfe2b10f26d215557ca04661170f0072578d75
NicsonSouza <nick.nicson@mgmail.com>
2022-01-04 11:14:14 -0300

    11.14

--- index.php
+++ index.php
@@ -32,61 +32,63 @@ if (isset($_GET['num1'], $_GET['num2'], $_GET['operacao'])) {
             <label for="">Primeiro número:</label>
             <input type="number" name="num1" value=<?= $num1 ?> require> <br>
             <select name="operacao" id="" require>
-                <option value="somar">Somar</option>
-                <option value="subtrair">Subtrair</option>
-                <option value="multiplicar">Multiplicar</option>
-                <option value="dividir">Dividir</option>
-                <option value="resto">Resto</option>
+                <option value="somar" <?= ($op == 'somar') ? 'Selected' : '' ?>>Somar</option>
+                <option value="subtrair" <?= ($op == 'subtrair') ? 'Selected' : '' ?>>Subtrair</option>
+                <option value="multiplicar" <?= ($op == 'multiplicar') ? 'Selected' : '' ?>>Multiplicar</option>
+                <option value="dividir" <?= ($op == 'dividir') ? 'Selected' : '' ?>>Dividir</option>
+                <option value="resto" <?= ($op == 'resto') ? 'Selected' : '' ?>>Resto</option>
             </select><br>
             <label for="">Segundo número:</label>
             <input type="number" name="num2" value=<?= $num2 ?> require> <br>
             <button type="submit">Calcular</button>
         </form>
     </div>
-    
-    <?php
+
+    <div class="result">
+        <?php
         switch ($op) {
 
             case 'somar':
                 $resultado = $num1 + $num2;
-                echo "<br>Resultado = {$resultado}";
+                echo "Resultado = {$resultado}";
                 break;
-    
+
             case 'subtrair':
                 $resultado = $num1 - $num2;
-                echo '<br>Resultado = ' . $resultado;
+                echo 'Resultado = ' . $resultado;
                 break;
-    
+
             case 'multiplicar':
                 $resultado = $num1 * $num2;
-                echo '<br>Resultado = ' . $resultado;
+                echo 'Resultado = ' . $resultado;
                 break;
-    
+
             case 'dividir':
                 $resultado = $num1 / $num2;
                 if ($num1 == 0) {
-                    echo '<br>Preencha o campo "Primeiro número" diferente de 0';
+                    echo 'Preencha o campo "Primeiro número" diferente de 0';
                 } else {
-                    echo '<br>Resultado = ' . $resultado;
+                    echo 'Resultado = ' . $resultado;
                 };
                 break;
-    
+
             case 'resto':
                 $resultado = $num1 % $num2;
                 if ($num1 == 0) {
-                    echo '<br>Preencha o campo "Primeiro número" diferente de 0';
+                    echo 'Preencha o campo "Primeiro número" diferente de 0';
                 } else {
-                    echo '<br>Resultado = ' . $resultado;
+                    echo 'Resultado = ' . $resultado;
                 };
                 break;
         }
 
-    if ($num1 == 0 && $num2 == 0) {
-        echo '<br>Os dados não foram preenchidos';
-    } else {
-        echo '<br>Dados recebidos';
-    };
-    ?>
+        if ($num1 == 0 && $num2 == 0) {
+            echo '<br><br>Os dados não foram preenchidos';
+        } else {
+            echo '<br><br>Dados recebidos';
+        };
+        ?>
+    </div>
 
 </body>
 
